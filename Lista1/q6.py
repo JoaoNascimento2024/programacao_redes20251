3c8950439714ae6ea3be0dd6202ea352ff1b14e7
João <162920587+JoaoNascimento2024@users.noreply.github.com>
2025-05-09 22:12:06 -0300

    Mudança simples

--- Lista1/q6.py
+++ Lista1/q6.py
@@ -3,6 +3,6 @@ for i in range(5):
     nome = input("Digite um nome de servidor")
     nomes.append(nome)
 
-print("Servidores digitados: ")
+print("Servidores digitados: XXX ")
 for nome in nomes:
     print(nome)
