e46e5141f0868c49007ad94a728e0bf4538febb5
Joao Maria Araujo do Nascimento <1724026@ifrn.local>
2025-05-09 22:15:25 -0300

    Teste de push

--- Lista1/q6.py
+++ Lista1/q6.py
@@ -3,6 +3,6 @@ for i in range(5):
     nome = input("Digite um nome de servidor")
     nomes.append(nome)
 
-print("Servidores digitados: XXX ")
+print("Servidores digitados: XXXX ")
 for nome in nomes:
     print(nome)
